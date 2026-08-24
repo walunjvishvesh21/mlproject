6278e5128d0bafa9734618c87f5a1efd5d970714
walunjvishvesh21 <79275988+walunjvishvesh21@users.noreply.github.com>
2026-08-24 16:26:25 +0530

    DATA INGESTION

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,52 @@
+import os 
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+
+
+@dataclass
+
+class DataIngestionConfig:
+    train_data_path : str = os.path.join('artifacts','train.csv')
+    test_data_path : str= os.path.join('artifacts','test_csv')
+    raw_data_path :str = os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('ENTERED THE DATA INGESTION')
+
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('READ THE DATASET AS DATAFRAME')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("TRAIN TEST SPLIT INITIALIZED")
+
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+
+            logging.info("INGESTION OF THE DATA IS COMPLETED")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
